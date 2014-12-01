d72eccba72ac54f14b9b969685c2a4558105e0fa
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-01 18:05:00 +0100

    refactoring birdy ... build process options on demand

--- birdy/__init__.py
+++ birdy/__init__.py
@@ -1,12 +1,11 @@
 """
-Generic wps client
+WPS command line client
+
+see help:
+$ birdy -h
 """
 
 import logging
-from owslib.wps import WebProcessingService
-
-SERVICE = "http://localhost:8094/wps"
-
 
 def execute(wps, args):
     inputs = []
@@ -62,17 +61,19 @@ def parse_default(input):
     return default
 
 def parse_description(input):
-    description = input.title
+    description = ''
+    if hasattr(input, 'title') and input.title is not None:
+        description = str(input.title)
     if hasattr(input, 'abstract'):
-        description = description + ": " + input.abstract
+        description = description + ": " + str(input.abstract)
     default = parse_default(input)
     if is_complex_data(input):
         if len(input.supportedValues) > 0: 
-            mime_types = ",".join([value.mimeType for value in input.supportedValues])
+            mime_types = ",".join([str(value.mimeType) for value in input.supportedValues])
             description = description + ", mime types=" + mime_types
     if default is not None:
-        description = description + " (default: " +  default + ")"
-    return description
+        description = description + " (default: " + str(default) + ")"
+    return description.encode(encoding='ascii')
 
 def parse_type(input):
     # TODO: see https://docs.python.org/2/library/argparse.html#type
@@ -110,7 +111,20 @@ def parse_nargs(input):
     elif input.minOccurs == 1:
         nargs == 1
     return nargs
-        
+
+def parse_process_help(process):
+    help = ''
+    if hasattr(process, "title"):
+        help = help + str(process.title) + ": "
+    if hasattr(process, "abstract"):
+        help = help + str(process.abstract)
+    return help.encode(encoding='ascii')
+
+def parse_wps_description(wps):
+    description="{0}: {1}".format(
+        wps.identification.title,
+        wps.identification.abstract)
+
 def create_parser(wps):
     """
     Generates commands to execute WPS processes on the command line.
@@ -123,49 +137,79 @@ def create_parser(wps):
     """
     
     import argparse
+    import sys
 
     parser = argparse.ArgumentParser(
-        prog="birdy",
-        description="%s: %s" % (wps.identification.title, wps.identification.abstract))
+        usage='''birdy [-h] <command> [<args>]''',
+        description=parse_wps_description(wps),
+        )
     subparsers = parser.add_subparsers(
         dest='identifier',
-        title='subcommands',
+        title='command',
         description='valid subcommands',
-        help='additional help')
-    for i in range(0, len(wps.processes)):
-        process = wps.describeprocess(wps.processes[i].identifier)
-        parser_process = subparsers.add_parser(process.identifier, help=process.abstract)
-
-        for input in process.dataInputs:
-            parser_process.add_argument('--'+input.identifier,
-                                        dest=input.identifier,
-                                        required=parse_required(input),
-                                        nargs=parse_nargs(input),
-                                        #type=parse_type(input),
-                                        choices=parse_choices(input),
-                                        default=parse_default(input),
-                                        action="store",
-                                        help=parse_description(input),
-                                )
-
-        output_choices = [output.identifier for output in process.processOutputs]
-        help_msg = "Output: "
-        for output in process.processOutputs:
-            help_msg = help_msg + output.identifier + "=" + parse_description(output) + " (default: output)"
-        parser_process.add_argument(
-            '--output',
-            dest="output",
-            default="output",
-            choices=output_choices,
-            action="store",
-            help=help_msg
+        help='additional help'
         )
+    for process in wps.processes:
+        #process = wps.describeprocess(wps.processes[i].identifier)
+        parser_process = subparsers.add_parser(
+            process.identifier,
+            #help=parse_process_help(process)
+            )
+
+        
+
+    args = parser.parse_args(sys.argv[1:2])
+    print 'Arguments:', args
+    process = wps.describeprocess(args.identifier)
+    parser_process = subparsers.add_parser(
+        process.identifier,
+        #help=parse_process_help(process)
+        )
+
+    for input in process.dataInputs:
+        parser_process.add_argument(
+            '--'+input.identifier,
+            dest=input.identifier,
+            #required=parse_required(input),
+            #nargs=parse_nargs(input),
+            #type=parse_type(input),
+            #choices=parse_choices(input),
+            #default=parse_default(input),
+            action="store",
+            #help=parse_description(input),
+        )
+    output_choices = [output.identifier for output in process.processOutputs]
+    help_msg = "Output: "
+    for output in process.processOutputs:
+       help_msg = help_msg + str(output.identifier) + "=" + parse_description(output) + " (default: output)"
+    parser_process.add_argument(
+        '--output',
+        dest="output",
+        default="output",
+        choices=output_choices,
+        action="store",
+        help=help_msg
+    )
+    args = parser.parse_args(sys.argv[1:])
+    print 'Arguments:', args
+    sys.exit(1)
         
     return parser
 
 def main():
+    from sys import exit
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
-    wps = WebProcessingService(SERVICE, verbose=False, skip_caps=False)
+
+    from os import environ 
+    service = environ.get("WPS_SERVICE", "http://localhost:8094/wps")
+    logging.debug('using wps %s', service)
+
+    from owslib.wps import WebProcessingService
+    try:
+        wps = WebProcessingService(service, verbose=False, skip_caps=False)
+    except:
+        logging.exception('Could not access wps %s', service)
+        exit(1)
 
     parser = create_parser(wps)
     args = parser.parse_args()
