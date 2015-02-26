2d73009afb951e3339fd1c427550780cf8f4b1de
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-02-26 15:49:36 +0100

    update wpsparser unit tests

--- unit_tests/test_wpsparser.py
+++ unit_tests/test_wpsparser.py
@@ -6,7 +6,10 @@ from __init__ import TESTDATA, WpsTestCase
 
 from birdy.wpsparser import (
     parse_wps_description,
-    parse_process_help
+    parse_process_help,
+    is_complex_data,
+    parse_default,
+    parse_description
     )
 
 class WPSParserTestCase(WpsTestCase):
@@ -21,4 +24,24 @@ class WPSParserTestCase(WpsTestCase):
         process = self.wps.describeprocess('helloworld')
         result = parse_process_help(process)
         nose.tools.ok_('Hello' in result, result)
+
+    @attr('online')
+    def test_is_complex_data(self):
+        process = self.wps.describeprocess('helloworld')
+        for input in process.dataInputs:
+            nose.tools.ok_(is_complex_data(input) == False)
+
+    @attr('online')
+    def test_parse_default(self):
+        raise SkipTest
+        process = self.wps.describeprocess('helloworld')
+        for input in process.dataInputs:
+            nose.tools.ok_(False, parse_default(input))
+
+    @attr('online')
+    def test_parse_description(self):
+        process = self.wps.describeprocess('helloworld')
+        for input in process.dataInputs:
+            result = parse_description(input)
+            nose.tools.ok_(len(result), result)
         
