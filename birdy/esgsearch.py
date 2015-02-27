a0ab0ded119041feaa5d0e6957bb3419cd9eb910
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-02-27 20:13:33 +0100

    working on argcomplete

--- birdy/esgsearch.py
+++ birdy/esgsearch.py
@@ -6,6 +6,9 @@ logger = logging.getLogger(__name__)
 # see completer examples:
 # https://github.com/kislyuk/argcomplete/blob/master/argcomplete/completers.py
 
+# see argcomplete usage:
+# https://gist.github.com/bewest/1202975
+
 def esgf_search_projects(prefix, parsed_args, **kwargs):
     choices = ("CMIP5", "CORDEX")
     return (choice for choice in choices if choice.startswith(prefix))
@@ -37,19 +40,35 @@ class ESGSearch(object):
             prog="esgsearch",
             #usage='''esgsearch [<options>] <command> [<args>]''',
             description="Query ESGF search",
+            add_help=False,
             )
-        parser.add_argument("--debug",
-                            help="enable debug mode",
-                            action="store_true")
+      
         parser.add_argument("--project", required=True).completer = esgf_search_projects
         parser.add_argument("--experiment", required=True).completer = esgf_search_experiments
         parser.add_argument("--variable", nargs="*").completer = esgf_search_variables
 
         subparsers = parser.add_subparsers(dest='command', title='commands')
         subparser = subparsers.add_parser("files", prog="esgsearch {0}".format("command"))
+
+       
+
+        # first sniff for basic debuggery, help fill in defaults
+        ## try:
+        ##     known, args = parser.parse_known_args()
+        ## except:
+        ##     parser.print_help( ) 
+
+        real_parser = argparse.ArgumentParser(add_help=False)
+
+        real_parser.add_argument("--project", required=True).completer = esgf_search_projects
+        real_parser.add_argument("--experiment", required=True).completer = esgf_search_experiments
+        real_parser.add_argument("--variable", nargs="*").completer = esgf_search_variables
+
+        subparsers = real_parser.add_subparsers(dest='command', title='commands')
+        subparser = subparsers.add_parser("files", prog="esgsearch {0}".format("command"))
         subparser.add_argument("--test")
 
-        return parser
+        return parser, real_parser
 
 def main():
     import argcomplete
@@ -58,9 +77,9 @@ def main():
     logger.setLevel(logging.INFO)
     
     esgsearch = ESGSearch()
-    parser = esgsearch.create_parser()
+    parser,real_parser = esgsearch.create_parser()
     argcomplete.autocomplete(parser)
-    args = parser.parse_args()
+    args = real_parser.parse_args()
 
 if __name__ == '__main__':
     sys.exit(main())
