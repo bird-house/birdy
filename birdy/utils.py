afe02f0021d8094dacea14c6b36a08dc8c497159
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-12 17:36:11 +0200

    fixed abspath in fix_local_url

--- birdy/utils.py
+++ birdy/utils.py
@@ -8,11 +8,12 @@ def fix_local_url(url):
     u = urlparse.urlsplit(url)
     if not u.scheme:
         # build local file url
-        if u.path.startswith('/'):
+        path = u.path.strip()
+        if path.startswith('/'):
             # absolute path
-            url = urlparse.urljoin('file://', u.path)
+            url = urlparse.urljoin('file://', path)
         else:
             # relative path
-            url = urlparse.urljoin('file://', abspath(join(curdir, u.path)))
+            url = urlparse.urljoin('file://', abspath(path))
         logger.debug("fixed url = %s", url)
     return url
--- unit_tests/test_utils.py
+++ unit_tests/test_utils.py
@@ -12,9 +12,20 @@ def test_fix_local_url():
     nose.tools.ok_(url.startswith('file:///'), url)
     nose.tools.ok_(url.endswith('testfile.nc'), url)
 
-    url = utils.fix_local_url('/home/data/testfile.nc')
+    url = utils.fix_local_url('/tmp/data/testfile.nc')
+    nose.tools.ok_(url == 'file:///tmp/data/testfile.nc', url)
+
+    url = utils.fix_local_url('  /home/data/testfile.nc  ')
     nose.tools.ok_(url == 'file:///home/data/testfile.nc', url)
 
-    url = utils.fix_local_url('../testfile.nc')
+    url = utils.fix_local_url('../../testfile.nc')
     nose.tools.ok_(url.startswith('file:///'), url)
     nose.tools.ok_(url.endswith('testfile.nc'), url)
+
+    url = utils.fix_local_url('  ../../testfile.nc  ')
+    nose.tools.ok_(url.startswith('file:///'), url)
+    nose.tools.ok_(url.endswith('testfile.nc'), url)
+
+    # TODO: replace ~
+    #url = utils.fix_local_url('~/data/testfile.nc')
+    #nose.tools.ok_(False, url)
