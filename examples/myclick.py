91131a5ade6d579fdb5e813dd778789c114429bd
pingu <pingu@norah>
2015-09-29 16:53:14 +0200

    working on click multi command example

--- examples/myclick.py
+++ examples/myclick.py
@@ -1,8 +1,21 @@
 import click
 
-@click.command()
-def hello():
-    click.echo('Hello World!')
+class MyCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        cmds = ['hello', 'inout']
+        return cmds
+
+    def get_command(self, ctx, name):
+        ns = {}
+        @click.command()
+        @click.option('--count', default=1, help='Number of greetings.')
+        def cmd(*args, **kwargs):
+            click.echo('hello')
+        return cmd
+
+cli = MyCLI(help='This tool\'s subcommands are loaded from a plugin folder dynamically.')
 
 if __name__ == '__main__':
-    hello()
+    cli()
+
+
