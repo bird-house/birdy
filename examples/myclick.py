226bb91cb75202d70ced1cde729aa39c1dbf408d
pingu <pingu@norah>
2015-09-29 18:04:20 +0200

    working on click example ...

--- examples/myclick.py
+++ examples/myclick.py
@@ -1,19 +1,39 @@
 import click
 
+def option(*param_decls, **attrs):
+    """Attaches an option to the command.  All positional arguments are
+    passed as parameter declarations to :class:`Option`; all keyword
+    arguments are forwarded unchanged (except ``cls``).
+    This is equivalent to creating an :class:`Option` instance manually
+    and attaching it to the :attr:`Command.params` list.
+
+    :param cls: the option class to instantiate.  This defaults to
+                :class:`Option`.
+    """
+    def decorator(f):
+        if 'help' in attrs:
+            attrs['help'] = inspect.cleandoc(attrs['help'])
+        OptionClass = attrs.pop('cls', Option)
+        _param_memo(f, OptionClass(param_decls, **attrs))
+        return f
+
+
 class MyCLI(click.MultiCommand):
     def list_commands(self, ctx):
         cmds = ['hello', 'inout']
         return cmds
 
     def get_command(self, ctx, name):
-        ns = {}
         @click.command()
-        @click.option('--count', default=1, help='Number of greetings.')
+        @option("--count", help="no help")
         def cmd(*args, **kwargs):
-            click.echo('hello')
+            click.echo(name)
+        #opt = click.Option("--count", help="no help")
+        #cmd.__click_params__.append(option)
+        
         return cmd
 
-cli = MyCLI(help='This tool\'s subcommands are loaded from a plugin folder dynamically.')
+cli = MyCLI(help='This tool\'s subcommands are loaded dynamically.')
 
 if __name__ == '__main__':
     cli()
