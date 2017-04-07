7d900f6060603e3dd229c74680321d0657c82db5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-07 14:58:29 +0200

    fixed wps_parser test

--- tests/test_wpsparser.py
+++ tests/test_wpsparser.py
@@ -23,26 +23,26 @@ class WPSParserTestCase(WpsTestCase):
 
     @attr('online')
     def test_parse_process_help(self):
-        process = self.wps.describeprocess('helloworld')
+        process = self.wps.describeprocess('hello')
         result = parse_process_help(process)
         nose.tools.ok_('Hello' in result, result)
 
     @attr('online')
     def test_is_complex_data(self):
-        process = self.wps.describeprocess('helloworld')
+        process = self.wps.describeprocess('hello')
         for input in process.dataInputs:
             nose.tools.ok_(is_complex_data(input) is False)
 
     @attr('online')
     def test_parse_default(self):
         raise SkipTest
-        process = self.wps.describeprocess('helloworld')
+        process = self.wps.describeprocess('hello')
         for input in process.dataInputs:
             nose.tools.ok_(False, parse_default(input))
 
     @attr('online')
     def test_parse_description(self):
-        process = self.wps.describeprocess('helloworld')
+        process = self.wps.describeprocess('hello')
         for input in process.dataInputs:
             result = parse_description(input)
             nose.tools.ok_(len(result), result)
