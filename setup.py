dc553cea736d5fc6fbc6b4979240cb18204a7bb8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-10 16:46:27 +0200

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 from setuptools import setup
 
 version = '0.1.9'
-description = 'Birdy provides a command-line tool to work with Web Processing Services (WPS).'
+description = 'Birdy provides a command-line tool to work with Web Processing Services.'
 long_description = (
     open('README.rst').read() + '\n' +
     open('AUTHORS.rst').read() + '\n' +
     open('CHANGES.rst').read()
 )
 
-requires = [line.strip() for line in open('requirements/deploy.txt')]
+requires = [line.strip() for line in open('requirements.txt')]
 
 classifiers = [
     'Development Status :: 3 - Alpha',
@@ -28,8 +28,9 @@ setup(name='birdhouse-birdy',
       description=description,
       long_description=long_description,
       classifiers=classifiers,
-      keywords='wps pywps python birdy netcdf esgf birdhouse anaconda',
+      keywords='wps pywps python birdy esgf birdhouse',
       author='Birdhouse',
+      author_email="wps-dev@dkrz.de",
       url='https://github.com/bird-house/birdy',
       license="Apache License v2.0",
       packages=find_packages(),
