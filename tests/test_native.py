09bdefa5bba93170150f2e840874adcfbd6902be
Pingu Carsti <ehbrecht@dkrz.de>
2018-06-14 16:30:38 +0200

    pep8

--- tests/test_native.py
+++ tests/test_native.py
@@ -1,12 +1,8 @@
 import pytest
 from birdy import native_client
 
+
 def test_BirdyMod():
     m = native_client(url="http://localhost:5000/wps")
     assert m.hello('david') == 'Hello david'
     assert m.binaryoperatorfornumbers(inputa=1, inputb=2, operator='add') == 3.0
-
-
-
-
-
