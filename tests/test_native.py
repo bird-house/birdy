2949532580221121c656038a18f75a4870092f47
David Huard <david.huard@gmail.com>
2018-06-19 09:09:26 -0400

    Created a config class to allow users to modify module behavior (#38)
    Added a config to modify module behavior and work on object conversion of results (#37)

--- tests/test_native.py
+++ tests/test_native.py
@@ -1,11 +1,15 @@
 import pytest
 import json
 from birdy import native_client
+from birdy import native
+
+# This tests assumes Emu is running on the localhost
+url = "http://localhost:5000/wps"
 
 
 @pytest.mark.online
-def test_birdymod():
-    m = native_client(url="http://localhost:5000/wps")
+def test_birdmod():
+    m = native_client(url=url)
     assert m.hello('david') == 'Hello david'
     assert m.binaryoperatorfornumbers(inputa=1, inputb=2, operator='add') == 3.0
     assert m.dummyprocess(10, 20) == ['11', '19']
@@ -14,5 +18,53 @@ def test_birdymod():
 
     assert type(txt) == str
     assert type(ref) == str
-    doc = json.loads(ref)
-    assert type(doc) == dict
+
+    m._config.asobject = True
+    txt, ref = m.multiple_outputs(2)
+    assert type(txt) == str
+    assert type(ref) == dict
+
+
+@pytest.mark.online
+def test_only_one():
+    m = native_client(url=url, processes=['nap'])
+    assert count_mod_func(m) == 1
+
+    m = native_client(url=url, processes='nap')
+    assert count_mod_func(m) == 1
+
+
+@pytest.mark.online
+def test_netcdf():
+
+    import netCDF4 as nc
+    if nc.getlibversion() > '4.5':
+        m = native_client(url=url, processes=['output_formats'], asobject=True)
+        ncdata, jsondata = m.output_formats()
+        assert isinstance(ncdata, nc.Dataset)
+        ncdata.close()
+        assert isinstance(jsondata, dict)
+
+
+def count_mod_func(mod):
+    import types
+    return len([f for f in mod.__dict__.values() if isinstance(f, types.FunctionType)])
+
+
+def test_converter():
+    j = native.JSONConverter()
+    assert j.default == 'json'
+
+
+def test_jsonconverter():
+    d = {"a": 1}
+    s = json.dumps(d)
+
+    j = native.JSONConverter()
+    assert j.json(s) == d
+
+
+def test_config():
+    c = native.Config()
+    c.asobject = 1
+    assert isinstance(c.asobject, bool)
