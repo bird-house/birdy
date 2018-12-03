59e6f10059dc9eb3c66aa6193be88da5312abb0f
David Caron <dcaron05@gmail.com>
2018-12-03 13:13:20 -0500

    test: simply run emu

--- tests/test_client.py
+++ tests/test_client.py
@@ -28,15 +28,16 @@ def start_emu(request):
         from emu.cli import cli
         sys.argv = [sys.executable, 'start', '--log-file', log_file]
         cli()
-    p = Process(target=run)
-    p.start()
+    # p = Process(target=run)
+    # p.start()
 
-    def finalizer():
-        p.terminate()
-        print("--------\nEmu Logs\n--------")
-        print(open(log_file).read())
+    # def finalizer():
+    #     p.terminate()
+    #     print("--------\nEmu Logs\n--------")
+    #     print(open(log_file).read())
 
-    request.addfinalizer(finalizer)
+    # request.addfinalizer(finalizer)
+    run()
 
 
 @pytest.fixture(scope="module")
