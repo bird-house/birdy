23cc557ae933370cd5b747116aa7171076a99c54
f-PLT <pelletier.f@gmail.com>
2020-10-01 10:35:58 -0400

    Extract conditional formatting from templating
    Each process should just receive the right output argument,
    parsing the inputs should be done by the cli.

--- birdy/cli/run.py
+++ birdy/cli/run.py
@@ -26,11 +26,30 @@ def _set_language(ctx, param, value):
     CONTEXT_OBJ['language'] = value
 
 
+def _format_outputs(ctx, param, output_obj):
+    if output_obj:
+        output = output_obj[0]
+        ref = output_obj[1].lower()
+        mime = output_obj[2]
+        as_ref = None
+        if ref == "true":
+            as_ref = True
+        elif ref == "false":
+            as_ref = False
+        mimetype = mime if mime.lower() != "none" else None
+        formatted_output = [(output, as_ref, mimetype)]
+        print(formatted_output)
+        CONTEXT_OBJ['outputs'] = formatted_output
+
+
 @click.command(cls=BirdyCLI, context_settings=CONTEXT_SETTINGS,
                url="http://localhost:5000/wps")
 @click.version_option()
 @click.option('--cert', help="Client side certificate containing both certificate and private key.")
-@click.option('--output', help="Output format", nargs=2)
+@click.option('--outputs', expose_value=False, nargs=3, callback=_format_outputs,
+                           help="Modify output format (optional). Takes three arguments, output name, "
+                                "as_reference (True, False, or None for process default), and mimetype"
+                                "(None for process default).")
 @click.option('--send', '-S', is_flag=True, help="Send client side certificate to WPS. Default: false")
 @click.option("--sync", '-s', is_flag=True, help="Execute process in sync mode. Default: async mode.")
 @click.option("--token", "-t", help="Token to access the WPS service.")
@@ -41,7 +60,7 @@ def _set_language(ctx, param, value):
     "--show-languages", "-L", expose_value=False, is_flag=True, is_eager=True, callback=_show_languages,
     help="Show a list of accepted languages for the WPS service.")
 @click.pass_context
-def cli(ctx, cert, output, send, sync, token):
+def cli(ctx, cert, send, sync, token):
     """
     Birdy is a command line client for Web Processing Services.
 
@@ -54,4 +73,3 @@ def cli(ctx, cert, output, send, sync, token):
     ctx.obj['send'] = send
     ctx.obj['sync'] = sync
     ctx.obj['token'] = token
-    ctx.obj['output'] = output
